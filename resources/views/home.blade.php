e6003af9be1c881f2a2cc084bb96a4544d691789
rahulkandel7 <rahulkandel7@gmail.com>
2021-06-08 20:13:40 +0545

    upto product overview

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,56 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
+{{-- Slide Show Content --}} 
+<div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+    
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+      <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="https://images.unsplash.com/photo-1623003641967-c43abbede243?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1502&q=80" class="d-block w-100 img-fluid" alt="...">
+        <div class="carousel-caption d-md-block">
+          <h5 class="animate__animated animate__backInDown">Summer Sale</h5>
+          <p class="animate__animated animate__heartBeat animate__delay-1s">Some representative placeholder content for the first slide.</p>
         </div>
+      </div>
+      <div class="carousel-item">
+        <img src="https://images.unsplash.com/photo-1599940676457-cae80eebdcd6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1500&q=80" class="d-block w-100 img-fluid" alt="...">
+        <div class="carousel-caption d-md-block">
+          <h5 class="animate__animated animate__backInDown">Second slide label</h5>
+          <p class="animate__animated animate__heartBeat animate__delay-1s">Some representative placeholder content for the second slide.</p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="https://images.unsplash.com/photo-1599834017599-d540b5fa8cf1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1500&q=80" class="d-block w-100 img-fluid" alt="...">
+        <div class="carousel-caption d-md-block">
+          <h5 class="animate__animated animate__backInDown">Third slide label</h5>
+          <p class="animate__animated animate__heartBeat animate__delay-1s">Some representative placeholder content for the third slide.</p>
+        </div>
+      </div>
     </div>
-</div>
+    <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+
+  {{-- Choose Section --}}
+  <div class="container">
+      <div class="row">
+          <div class="banner col-md-6">
+              Women Sales Banner
+          </div>
+          <div class="banner col-md-6">
+            Male Sales Banner
+          </div>
+      </div>
+  </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,66 +18,157 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+
+
+    <!-- Fontawesome Links -->
+    <script src="https://kit.fontawesome.com/535ccb550f.js" crossorigin="anonymous"></script>
+
+    <!-- Animate Css -->
+    <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+  />
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        
+
+        {{-- Navbar --}}
+        <nav class="navbar navbar-expand-md navbar-light shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
+                <nav class="navbar navbar-light bg-light">
+                    <div class="container">
+                      <a class="navbar-brand" href="#">
+                        <img src="https://www.fisu.net/medias/images/fisu_logo_2020_color_square.png?v=52" alt="logo" width="50" height="40">
+                      </a>
+                    </div>
+                  </nav>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
+                <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                          <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="#">Shop</a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="#">About Us</a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="#">Contact Us</a>
+                          </li>
+                          
+                        </ul>
+                        <ul class="navbar-nav ml-auto">
+                            <!-- Authentication Links -->
+                            @guest
+                                @if (Route::has('login'))
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                    </li>
+                                @endif
+    
+                                @if (Route::has('register'))
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    </li>
+                                @endif
+                            @else
+                                <div class="ico">
+                                    <li class="nav-item mr-2">
+                                        <a href="" class="nav-link nav-icons"><i class="fab fa-opencart"></i></a>
+                                    </li>
+                                    <div class="num">
+                                        0
+                                    </div>
                                 </div>
-                            </li>
-                        @endguest
-                    </ul>
+
+                                <div class="ico">
+                                    <li class="nav-item mr-2">
+                                        <a href="" class="nav-link nav-icons"><i class="far fa-heart"></i></a>
+                                    </li>
+                                    <div class="num">
+                                        9
+                                    </div>
+                                </div>
+                                
+                                <li class="nav-item dropdown">
+                                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                        {{ Auth::user()->name }}
+                                    </a>
+    
+                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="{{ route('logout') }}"
+                                           onclick="event.preventDefault();
+                                                         document.getElementById('logout-form').submit();">
+                                            {{ __('Logout') }}
+                                        </a>
+    
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                            @csrf
+                                        </form>
+                                    </div>
+
+                                    
+                                </li>
+                                
+                            @endguest
+                        </ul>
+                        
+
                 </div>
             </div>
         </nav>
 
-        <main class="py-4">
+        <main>
             @yield('content')
         </main>
     </div>
+
+    <footer>
+        <div class="container-fluid bg-dark">
+            <div class="container pt-4 pb-4">
+                <div class="row">
+                    <div class="col-md-4">
+                        <h3 class="footer-heading">Categories</h3>
+                        <ul class="navbar-nav">
+                            <li class="nav-item mb-2"> <a href="" class="foot-link">All</a></li>
+                            <li class="nav-item mb-2"> <a href="" class="foot-link">Mens</a></li>
+                            <li class="nav-item mb-2"> <a href="" class="foot-link">Womens</a></li>
+
+                        </ul>
+                    </div>
+                    <div class="col-md-4">
+                        <h3 class="footer-heading">Help</h3>
+                        <ul class="navbar-nav">
+                            <li class="nav-item mb-2"> <a href="" class="foot-link">About</a></li>
+                            <li class="nav-item mb-2"> <a href="" class="foot-link">Contact</a></li>
+                            <li class="nav-item mb-2"> <a href="" class="foot-link">My Cart</a></li>
+                            <li class="nav-item mb-2"> <a href="" class="foot-link">FAQs</a></li>
+
+                        </ul>
+                    </div>
+                    <div class="col-md-4">
+                        <h3 class="footer-heading">Get In Touch</h3>
+                        <p class="foot-info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima architecto sequi quibusdam quaerat 
+                            repellendus debitis accusantium enim earum eaque provident officiis quia rerum similique voluptatibus, 
+                            neque facere iusto fuga omnis.
+                        </p>
+                        <p>
+                            <span class="mr-3"><a href="www.facebook.com" class="foot-social"><i class="fab fa-facebook"></i></a></span>
+                            <span class="mr-3"><a href="" class="foot-social"><i class="fab fa-instagram" ></i></a></span>
+                            <span><a href="" class="foot-social"><i class="fab fa-tiktok"></i></a></span>
+
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
 </body>
 </html>
